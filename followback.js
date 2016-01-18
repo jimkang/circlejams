3ea9c3f4ce5b19e408cdcb44657c07b7e46ba8b2
Jim Kang <jimkang@gmail.com>
2016-01-18 12:38:45 -0500

    It's OK to unfollow now.

--- followback.js
+++ followback.js
@@ -5,16 +5,6 @@ filteredFollowback(
   {
     twitterCreds: config.twitter,
     neverUnfollow: [
-      3247937115,
-      2566358196,
-      3236234039,
-      3121911275,
-      3045096328,
-      2984664838,
-      2948121401,
-      3257074863,
-      4105513965,
-      1533777176
     ],
     blacklist: [
     ]
